ef111e46fb3989f3804d15964990af0789e109b0
Rodrigo Valente <rodrigo.araujo@tecsoft.info>
2018-03-16 14:32:44 -0300

    Updating repo with work done

---
+++ client/main.js
@@ -0,0 +1,35 @@
+// import { Template } from 'meteor/templating';
+// import { ReactiveVar } from 'meteor/reactive-var';
+// import 'semantic-ui-css/semantic.min.css';
+
+// import './main.html';
+
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
+
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
+
+import React from 'react';
+import { render } from 'react-dom';
+
+import { Meteor } from 'meteor/meteor';
+
+
+import Application from '../imports/ui/App';
+
+Meteor.startup(() => {
+  render(<Application />, document.getElementById('render-target'));
+});
---
+++ imports/ui/App.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Button, Container, Header, List } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
+export default class Application extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0 };
+
+    }
+
+    // Use arrow functions to auto bind this    
+    incrementCounter = () => {
+        this.setState({ counter: this.state.counter + 1 });
+    }
+
+    render() {
+        return (
+            <div className='ui one column centered grid'>
+                <div className='column'>
+                    <div className='row'>
+                        <Header as='h1'>Welcome to Meteor!</Header>
+                        <Button onClick={this.incrementCounter}>Click Me</Button>
+                        <p>You've pressed the button {this.state.counter} times.</p>
+                    </div>
+                    <div className='row'>
+                        <Header as='h2'>Learn Meteor!</Header>
+                        <List>
+                            <List.Item>
+                                <a href="https://www.meteor.com/try" target="_blank">Do the Tutorial</a>
+                            </List.Item>
+                            <List.Item>
+                                <a href="https://guide.meteor.com" target="_blank">Follow the Guide</a>
+                            </List.Item>
+                            <List.Item>
+                                <a href="https://docs.meteor.com" target="_blank">Read the Docs</a>
+                            </List.Item>
+                            <List.Item>
+                                <a href="https://forums.meteor.com" target="_blank">Discussions</a>
+                            </List.Item>
+                        </List>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
